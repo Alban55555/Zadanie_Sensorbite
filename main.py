818ee05da56aec3c3b4bc0bdefdc70b70e76a69f
aalban55555 <a.albanowski@campus.tu-berlin.de>
2025-12-05 19:41:34 +0100

    adding map

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+#
 # main.py
 import logging
+#import uvicorn
+#from fastapi import FastAPI
+
+#from app.api import router
+#from app.logging_conf import setup_logging
+
+#setup_logging()
+
+#app = FastAPI(title="Evacuation Routing Service")
+#app.include_router(router)
+
+#if __name__ == "__main__":
+ #   uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# main.py
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.api import router
 from app.logging_conf import setup_logging
@@ -11,5 +27,9 @@ setup_logging()
 app = FastAPI(title="Evacuation Routing Service")
 app.include_router(router)
 
+# NEW: serve static folder
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
